4919563b855972f8ee76acf837c1deda23e7fa40
Krithick Jegatheesan <j.krithick@gmail.com>
2025-02-11 20:49:27 +0530

    init-db, dockerfile, dockercompose, alembic reset

--- app/models/moderation.py
+++ app/models/moderation.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Column, String, Integer, JSON
 from app.configs.db_config import Base
+from sqlalchemy.orm import DeclarativeBase
 
+class Base(DeclarativeBase):
+    pass
 
 class ModerationResult(Base):
     __tablename__ = "moderation_results"
